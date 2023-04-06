c8272f39bf097ea11eef2dce2dba315cd7e1d0b7
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-04-06 15:41:19 +0900

    컴퓨터 자동으로 숫자 만드는 함수 구현 완료 <김현수>

--- baseball.js
+++ baseball.js
@@ -10,3 +10,26 @@
 // 게임을 종료합니다.
 
 
+// node.js readline library
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+// -- node.js readline library
+
+
+const comNum = autoNumberCreate(3);   // 컴퓨터가 3자리 숫자를 생성하는 함수 호출
+// 컴퓨터가 n자리 숫자를 생성하는 함수
+function autoNumberCreate(n) {
+    let arrNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let pickedNumbers = [];
+    let result = '';
+    for (let i = 0; i < n; i++) {
+        let randomIndex = Math.floor(Math.random() * arrNumbers.length);
+        pickedNumbers.push(arrNumbers[randomIndex]);
+        arrNumbers.splice(randomIndex, 1);
+    }
+    result = (pickedNumbers.join(''))
+    return result;
+}
